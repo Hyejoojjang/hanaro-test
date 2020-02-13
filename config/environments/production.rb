64b0122766a50c4d9c039ada4f4aa29ca1744308
hyejoo <hyejoojjang@likelion.org>
2020-02-13 18:33:02 +0900

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,6 +67,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "homework_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  config.action_mailer.perform_deliveries = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
