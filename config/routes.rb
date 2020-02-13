af8c2a8a41ed9ccdefde1ea274dd178db0018682
hyejoo <hyejoojjang@likelion.org>
2020-02-13 16:00:54 +0900

    sessions/new route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root 'welcome#welcome'
   match '/profile/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup_
   get 'welcome/menu' => 'welcome#menu', :as => :welcome_menu
+  get 'sessions/new' => 'sessions#new'
   get 'welcome/event' => 'welcome#event', :as => :welcome_event
   get 'welcome/mypage' => 'welcome#mypage', :as => :mypage
   get '/auth/kakao/callback' => 'welcome#welcome'
