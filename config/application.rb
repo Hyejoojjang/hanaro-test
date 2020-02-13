748b1c332db33e40f84538f46f11604a17217bf7
hyejoo <hyejoojjang@likelion.org>
2020-02-13 11:07:03 +0900

    test

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(:default, :assets, Rails.env)
 ## Enable the asset pipeline
 #config.assets.enabled = true
 ## Version of your assets, change this if you want to expire all your assets
-config.assets.version = '1.0'
+#config.assets.version = '1.0'
 
 module Hanaro
   class Application < Rails::Application
