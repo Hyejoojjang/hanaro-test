6bb5fc155660ef95990180ed0c9ec250f4cd9196
hyejoo <hyejoojjang@likelion.org>
2020-02-13 18:30:05 +0900

    test

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,8 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks' }
   root 'welcome#welcome'
   match '/profile/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup_
-  get 'welcome/menu' => 'welcome#menu', :as => :welcome_menu
-  get 'sessions/new' => 'sessions#new'
   get 'welcome/event' => 'welcome#event', :as => :welcome_event
   get 'welcome/mypage' => 'welcome#mypage', :as => :mypage
   get '/auth/kakao/callback' => 'welcome#welcome'
-  get 'users/sign_in' => 'users#sign_in', :as => :new_users_session_path
 
 end
