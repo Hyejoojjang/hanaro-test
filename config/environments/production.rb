cd4de7cc15370429172db876a954bca37ecbd2e3
hyejoo <hyejoojjang@likelion.org>
2020-02-13 09:16:31 +0900

    compile -> true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,9 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  APPLICATIONNAME::Application.configure do
+    config.assets.compile = true
+  end
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
